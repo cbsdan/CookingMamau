88bba655bdfb6c38f102d78435302e0dd50ec052
Daniel Cabasa <cabasadaniel1@gmail.com>
2024-07-16 08:59:42 +0800

    Add api and web routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\BakedGoodsController;
+use App\Http\Controllers\DiscountController;
+use App\Http\Controllers\IngredientController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +21,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware('auth:sanctum')->post('/logout', [UserController::class, 'logout'])->name('logout');
+Route::middleware('auth:sanctum')->get('/profile', [UserController::class, 'profile'])->name('profile');
+
+Route::post('/register', [UserController::class, 'registerUser'])->name('auth.register');
+Route::post('/login', [UserController::class, 'loginUser'])->name('auth.login');
+Route::put('/api/baked_goods/images/{imageId}/set-thumbnail', [BakedGoodsController::class, 'setThumbnail']);
+Route::delete('/baked_goods/images/{id}', [BakedGoodsController::class, 'deleteImage']);
+
+Route::apiResource('ingredients', IngredientController::class);
+Route::apiResource('baked_goods', BakedGoodsController::class);
+Route::apiResource('discounts', DiscountController::class);
