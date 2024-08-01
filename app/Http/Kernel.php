b3f71a8c0531ca7e91333129a32535a0399d7f57
Daniel Cabasa <cabasadaniel1@gmail.com>
2024-08-01 08:59:08 +0800

    register admin middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -36,6 +36,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
         ],
 
         'api' => [
@@ -56,6 +57,7 @@ class Kernel extends HttpKernel
     protected $middlewareAliases = [
         'LoginCheck' => \App\Http\Middleware\LoginCheck::class,
         'admin' => \App\Http\Middleware\AdminMiddleware::class,
+        'auth.admin' => \App\Http\Middleware\EnsureAdmin::class,
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
